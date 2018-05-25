d87f1d9c2df7e81d124f85972908ebdf1cea5b80
praton <praton@pratonian>
2018-05-25 20:09:05 +0530

    lot of changes a brand new rice

--- lock.sh
+++ lock.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-ICON=$HOME/.xlock/key.png
+
+#ICON=$HOME/.xlock/key.png
+
+ICON=$HOME/.i3/Locks/Minato_9_edited.png
 TMPBG=/tmp/screen.png
+SCALED_LOCK=/tmp/scaled_lock.png
+
 scrot /tmp/screen.png
+
+#convert $ICON -scale 60% -rotate 90 $SCALED_LOCK
+ 
+convert $ICON -scale 100% $SCALED_LOCK
+
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+
+convert $TMPBG $SCALED_LOCK -gravity center -composite -matte $TMPBG
+
 i3lock -u -i $TMPBG
---
+++ wall.sh
@@ -0,0 +1,6 @@
+#while true;do
+	#feh --bg-scale $(ls ~/Pictures/WP/*.jpg | shuf -n 1)
+	#sleep 60;
+#done
+feh --bg-scale ~/Pictures/WP/567918.png
+
