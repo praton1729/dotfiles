c89c9a7c8fea89d9385a264a1c6e0db070dba0bd
Prathu Baronia <prathubaronia2011@gmail.com>
2018-09-23 04:52:25 +0530

    modified polybar and added a test polybar config

--- rofi.sh
+++
@@ -1,2 +0,0 @@
-rofi -show run -lines 5 -eh 2 -width 100 -opacity "100" -padding 300 -font "System San Francisco Display 15" -color-window "#152233,#152233" -color-normal "#152233,#ffffff,#152233,#00ffaa,#4b0082"
-
--- wall.sh
+++
@@ -1,6 +0,0 @@
-#while true;do
-	#feh --bg-scale $(ls ~/Pictures/WP/*.jpg | shuf -n 1)
-	#sleep 60;
-#done
-feh --bg-scale ~/Pictures/WP/567918.png
-
