7df0de0bd9bb2a434a889245208a87b97251dba6
Prathu Baronia <prathu.baronia@oneplus.com>
2021-06-19 00:32:28 +0530

    Do a cleanup of the repo
    Signed-off-by: Prathu Baronia <prathu.baronia@oneplus.com>

--- locks/lock.sh
+++ locks/lock.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-#ICON=$HOME/.xlock/key.png
-
 ICON=$HOME/.i3/Locks/Minato_9_edited.png
 TMPBG=/tmp/screen.png
 SCALED_LOCK=/tmp/scaled_lock.png
 
 scrot /tmp/screen.png
 
-#convert $ICON -scale 60% -rotate 90 $SCALED_LOCK
- 
 convert $ICON -scale 100% $SCALED_LOCK
 
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
